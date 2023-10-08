107f438a7a51e490793c5434093ac9b3a5a72c5b
no-venv <>
2023-10-08 17:32:29 -0400

    update

--- home.js
+++ home.js
@@ -26,12 +26,3 @@ cat_img.onclick = function(){
     setTimeout(() => popup.fadeOut("slow"),1000)
  
 }
-
-async function set_pfp(){
-    const response = await fetch("https://raw.githubusercontent.com/no-venv/no-venv.github.io/main/pfp_location");
-    const pfp_location = await response.text();
-    const img_data = await (await fetch("https://corsproxy.io/?"+pfp_location)).arrayBuffer();
-    const base64_img = btoa(String.fromCharCode.apply(null,new Uint8Array(img_data)));
-    get("pfp_img").src = "data:image/png;base64,"+base64_img;
-}
-set_pfp()
