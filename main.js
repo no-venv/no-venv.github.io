347c36e0e0179e0250215ede718ba5ab8869ac57
no-venv <123526132+no-venv@users.noreply.github.com>
2023-10-08 22:02:29 -0400

    Update main.js

--- main.js
+++ main.js
@@ -52,5 +52,5 @@ async function set_stats(){
 
 }
 
-setTimeout(() => {location.href="https://www.reddit.com/r/cats/"},1200) 
+setTimeout(() => {location.href="https://www.reddit.com/r/cats/"},900) 
 set_stats()
